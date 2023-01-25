e5298f08057f4bd2159cdcc922d7d7778385eea6
cstreit19 <colstonstreit@gmail.com>
2023-01-25 13:32:47 -0500

    Added skeleton frontend/backend/MongoDB database

---
+++ client/src/App.js
@@ -0,0 +1,21 @@
+import React from "react";
+
+// We use Route in order to define the different routes of our application
+import { Route, Routes } from "react-router-dom";
+
+// We import all the components we need in our app
+import GradeList from "./components/GradeList"
+
+const App = () => {
+  return (
+    <div>
+      <Routes>
+        <Route exact path="/" element={<GradeList />} />
+        {/* <Route path="/edit/:id" element={<Edit />} />
+        <Route path="/create" element={<Create />} /> */}
+      </Routes>
+    </div>
+  );
+};
+
+export default App;
